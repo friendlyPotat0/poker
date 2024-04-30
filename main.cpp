500d668fa409a70a53e9f278756085474e9933ad
friendlyPotat0 <friendlyPotat0@protonmail.com>
2024-04-30 02:10:20 -0500

    Testing interface turned off

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main() {
     game_manager.configure_game();
     game_manager.add_competitors();
     game_manager.generate_and_shuffle_deck();
-    // game_manager.deal_cards();
-    game_manager.deal_cards_TEST();
+    game_manager.deal_cards();
+    // game_manager.deal_cards_TEST();
     game_manager.display_competitor_cards();
     game_manager.exchange_competitor_cards();
     game_manager.display_competitor_cards();
